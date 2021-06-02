afda2937ea00a5c0dee2b1d49870317dc60310a3
StudioChew <roope.aulis.oliver@gmail.com>
2021-06-02 12:52:14 +0300

    add messy dotfiles (please clean up later, future me)

---
+++ dotfiles/dmenu-5.0/grayscaleconfig.h
@@ -0,0 +1,23 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"Dina:pixelsize=13"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#b9b9b9", "#101010" },
+	[SchemeSel] = { "#b9b9b9", "#7c7c7c" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
